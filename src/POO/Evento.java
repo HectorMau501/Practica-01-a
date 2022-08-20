edbc63d3389e9a475269679d21f75c4afe79f80f
HectorMau501 <mauhector7@gmail.com>
2022-08-20 10:32:37 -0500

    Agregado de metodos y atributos

--- src/POO/Evento.java
+++ src/POO/Evento.java
@@ -13,6 +13,11 @@ public class Evento {
     
     //Metodos 
     
+    public Evento()
+    {
+        
+    }
+  
     public void setFecha(String fecha)
     {
         this.fecha = fecha;
@@ -21,17 +26,65 @@ public class Evento {
     {
         return  fecha;
     }
-
+    
+    public void setHora(String hora)
+    {
+        this.hora = hora;
+    }
+    public String getHora()
+    {
+        return hora;
+    }
+    
+    public void setlugar(String lugar)
+    {
+        this.lugar = lugar;
+    }
+    public String getLugar()
+    {
+        return lugar;
+    }
+    
+    public void setCapacidad(int capacidad)
+    {
+        this.capacidad = capacidad;
+    }
+    public int getCapacidad()
+    {
+        return capacidad;
+    }
+    
     public void setCostoBoleto(double costoBoleto) 
     {
         this.costoBoleto = costoBoleto;
     }
-    
     public double getCostoBoleto() 
     {
         return costoBoleto;
     }
-
+    
+    //Metodos Constructores
+    
+    public Evento(String fecha, String hora, String lugar, int capacidad, double costoBoleto)
+    {
+    this.fecha = fecha;
+    this.hora = hora;
+    this.lugar = lugar;
+    this.capacidad = capacidad;
+    this.costoBoleto = costoBoleto;
+    }
+    
+    //NOTA: Solo me permite definir un constructor
+    /*
+    public Evento()
+    {
+    fecha = "20 de Agosto del 2022";
+    hora = "10:30";
+    lugar = "Cocierto";
+    capacidad = 5000;
+    costoBoleto = 299.99;
+    }
+    */
     
     
     
